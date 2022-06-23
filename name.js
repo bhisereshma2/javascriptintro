d0ac03db5f3a1181a09fcbe7223582b5f029cd3c
bhisereshma2 <bhisereshma2@gmail.com>
2022-06-24 02:27:03 +0530

    first new commit

---
+++ name.js
@@ -0,0 +1,4 @@
+let myname="Reshma";
+let myheight=5.2;
+let mycountry="India";
+document.write("Hi! This is"+" "+" "+myname +"<br>"+ "My height is"+" "+" "+myheight +"<br>"+ "I am from"+" "+" "+mycountry);
